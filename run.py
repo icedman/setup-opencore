154033611798633b1ee315ae023d65ece87302da
icedman <m4rvin2005@gmail.com>
2021-07-06 20:20:26 +0800

    organize downloads

--- run.py
+++ run.py
@@ -7,8 +7,10 @@ import subprocess
 f = open('./sources.json')
 sources = json.load(f)
 
-def download(url, file):
-	extension = os.path.splitext(file)[1]
+def download(url, file, info):
+	sx = os.path.splitext(file)
+	filename = sx[0]
+	extension = sx[1]
 
 	if not os.path.isfile(file) and extension == '.zip':
 		subprocess.call(['wget', '-P', os.path.dirname(file), url])
@@ -20,8 +22,14 @@ def download(url, file):
 
 	if os.path.isfile(file) and extension == '.zip':
 		os.chdir(os.path.dirname(file))
-		subprocess.call(['unzip',  '-n', os.path.basename(file)])
-		os.chdir('../')
+
+		if not os.path.isdir(info['name']):
+			os.mkdir(info['name'])
+
+		os.chdir(info['name'])
+		subprocess.call(['unzip',  '-n', '../' + os.path.basename(file)])
+
+		os.chdir('../../')
 
 def prepare ():
 	if not os.path.isdir('./tools'):
@@ -35,21 +43,24 @@ def prepare ():
 def downloadTools ():
 	for t in sources['tools']:
 		target = os.path.join('./tools', os.path.basename(t['url']))
-		download(t['url'], target)
+		download(t['url'], target, t)
 
 	# apply our patches
-	subprocess.call(['patch', './tools/GenSMBIOS/GenSMBIOS.command', './patches/genbios.patch'])
+	subprocess.call(['patch', '-fs', './tools/GenSMBIOS/GenSMBIOS.command', './patches/genbios.patch'])
 	
 
 def downloadKexts ():
 	for t in sources['kexts']:
 		target = os.path.join('./kexts', os.path.basename(t['url']))
-		download(t['url'], target)
+		download(t['url'], target, t)
 
 def prepareBaseEFI ():
-	if not os.path.isdir('./EFI'):
-		subprocess.call(['cp',  '-R', './tools/X64/EFI', './'])
-		subprocess.call(['cp',  './tools/Docs/Sample.plist', './EFI/OC/config.plist'])
+	subprocess.call(['cp',  '-R', './tools/X64/EFI', './'])
+	subprocess.call(['cp',  './tools/Docs/Sample.plist', './EFI/OC/config.plist'])
 
-prepare()
-prepareBaseEFI()
+if not os.path.isdir('./tools') and not os.path.isdir('./kexts'):
+	prepare()
+
+if not os.path.isdir('./EFI'):
+	prepareBaseEFI()
+
